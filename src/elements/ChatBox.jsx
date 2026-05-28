830de32b04a4cb27c4506e876a90553d3c26b0f9
magsdfil <masval8284@gmail.com>
2026-05-28 17:48:55 +0300

    ssss

---
+++ src/elements/ChatBox.jsx
@@ -0,0 +1,12 @@
+import "../css/chatbox.css"
+function ChatBox(){
+
+    return (
+        <div className="chat-box-container">
+            <div className="picture-container">
+                <img src="https://abrakadabra.fun/uploads/posts/2021-12/1640847611_4-abrakadabra-fun-p-grustnie-avi-depressiya-10.jpg"></img>
+            </div>
+        </div>
+    )
+}
+export default ChatBox;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,5 @@
 import '../css/MainPage.css'
+import ChatBox from '../elements/ChatBox';
 import LeftBarList from '../icons/LeftBarList';
 function MainPage(){
     return (
@@ -7,7 +8,12 @@ function MainPage(){
             <div className='left-container'>
                 <LeftBarList />
             </div>
-            <div className='middle-container'></div>
+            <div className='middle-container'>
+                <div className='middle-container-up'></div>
+                <div className='middle-container-down'>
+                    <ChatBox></ChatBox>
+                </div>
+            </div>
             <div className='right-container'></div>
 
         </div>
